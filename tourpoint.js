becf85b0845cd26592358184381dd43a12c92989
Piyapattt <158018424+Piyapattt@users.noreply.github.com>
2024-05-13 16:47:16 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,171 @@
+var tourpoint= {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50257409907204,
+            14.330787044469119
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "cultural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49173529300322,
+            13.751018085710868
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Icon Siam",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.5104364999981,
+            13.726696908694592
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Asiatique",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.50410208465581,
+            13.705052024188518
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "King Taksin The Great Bridge",
+          "type": "sapcial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51199981349225,
+            13.719348830968954
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat arun",
+          "type": "cultural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.48891999999745,
+            13.74366848810061
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Golden Mount",
+          "type": "cultural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.50660708465716,
+            13.753813989184906
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Victory Monument",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.53826025066508,
+            13.764917446242933
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Suan Amporm Park",
+          "type": "spacial",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.5107252836811,
+            13.770004179318278
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Loha Prasat",
+          "type": "cultural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.50420627116495,
+            13.754882937370795
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Benchakitti Park",
+          "type": "park",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.55852054232952,
+            13.730636364563935
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
